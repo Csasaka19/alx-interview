a71c7bb6a833ff0664bbfdeef1c8f241ed06ef71
Clive Sasaka <clivesasaka@gmail.com>
2023-10-23 11:32:22 +0300

    Second look at the characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,25 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv > 2) {
-    request(url + process.argv[2], function (error, response, body) {
-        if (error) {
-        console.log(error);
-        } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-            request(character, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(JSON.parse(body).name);
-            }
-            });
-        }
-        }
-    });
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
 }
